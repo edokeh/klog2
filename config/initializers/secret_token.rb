8351533586f25a89a32b47c3bcbcfb4dc410f48c
edokeh <edokeh@163.com>
2013-12-16 16:27:08 +0800

    Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Klog2::Application.config.secret_key_base = '13f879a77cb72bc672eb9eb52d6811a46d9ecb33474588754c5552e3333f1e59d193afc197d2123e7b7b2a3be754a6a8adc803e0d6eaa519b9cc0209c9358321'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Klog2::Application.config.session_store :cookie_store, key: '_klog2_session'
