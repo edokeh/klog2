4a5d0921fc543dab4a182fb3e4fc0cc6b4e55296
edokeh <edokeh@163.com>
2013-12-29 23:49:42 +0800

    category & tag url

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,11 @@
+class CategoriesController < ApplicationController
+
+  def show
+    @category = Category.where(:name=>params[:id]).first
+    raise  ActiveRecord::RecordNotFound if @category.nil?
+
+    @blogs = @category.blogs.with_status(:publish).order("created_at DESC")#.page(params[:page])
+    render 'blogs/index'
+  end
+
+end
---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,9 @@
+class TagsController < ApplicationController
+
+  def show
+    @tag = ActsAsTaggableOn::Tag.where(:name => params[:id])
+    @blogs = Blog.with_status(:publish).tagged_with(params[:id]).order("created_at DESC")#.page(params[:page])
+    render 'blogs/index'
+  end
+
+end
