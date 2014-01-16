db9d2d6c9dbe415327faa09a58e5e2f42f8d37ed
edokeh <edokeh@163.com>
2014-01-16 23:22:01 +0800

    Add common form validation

--- app/assets/javascripts/admin/common/error-msg.js
+++ app/assets/javascripts/admin/common/error-msg.js
@@ -38,16 +38,42 @@ define(function (require, exports, module) {
                 return error;
             };
         }])
-        .directive('errorMsg', function () {
+        .directive('errorMsg',function () {
             return {
                 restrict: 'CA',
                 scope: {
                     field: '=errorMsg'
                 },
-                template: require('./template/error-msg.html'),
-                link: function (scope, element, attrs) {
+                template: require('./template/error-msg.html')
+            };
+        }).directive('formValid', ['ErrorMsg',function (ErrorMsg) {
+            return {
+                restrict: 'AC',
+                require: 'form',
+                link: function (scope, element, attrs, form) {
+                    attrs.$set('novalidate', 'novalidate');
 
+                    var formName = attrs.formValid || attrs.name;
+
+                    element.on('submit', function () {
+                        _.each(form, function (control, name) {
+                            if (name[0] !== '$') {
+                                control.$submitted = true;
+                            }
+                        });
+                        scope.$apply();
+                    });
+
+                    form.$setValidity = _.wrap(form.$setValidity, function (fn, validationToken, isValid, control) {
+                        fn.call(form, validationToken, isValid, control);
+
+                        if (!isValid) {
+                            control.$errorMessage = ErrorMsg[formName][control.$name][validationToken];
+                        } else {
+                            control.$errorMessage = null;
+                        }
+                    });
                 }
             };
-        });
+        }]);
 });
