75d9a056d438a02a1931e9d2e37d42bf21a4b2b3
edokeh <edokeh@163.com>
2014-01-21 23:20:38 +0800

    Add page module

--- Gemfile
+++ Gemfile
@@ -52,7 +52,7 @@ gem 'cells', '~> 3.9.1'
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
 # Use unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano', group: :development
--- app/assets/javascripts/admin/editor/factory/editor.js
+++ app/assets/javascripts/admin/editor/factory/editor.js
@@ -1,7 +1,7 @@
 /**
  * Markdown 编辑器
  */
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     var marked = require('gallery/marked/0.3.0/marked');
     var _ = require('_');
 
@@ -21,7 +21,7 @@ define(function(require, exports, module) {
         return _.contains(event.originalEvent.dataTransfer.types, 'Files');
     }
 
-    var Editor = ['Attach', '$modal', '$parse', function(Attach, $modal, $parse) {
+    var Editor = ['Attach', '$modal', '$parse', function (Attach, $modal, $parse) {
         var preview = true;
 
         return {
@@ -30,20 +30,20 @@ define(function(require, exports, module) {
              * @param $scope
              * @param options  {src: 'blog.content', dest: 'htmlContent'}
              */
-            addPreviewFn: function($scope, options) {
+            addPreviewFn: function ($scope, options) {
                 var setter = $parse(options.dest).assign;
 
                 // 监控变化生成预览
-                $scope.$watch(options.src, function(value) {
+                $scope.$watch(options.src, function (value) {
                     if (preview) {
-                        marked(value, function(err, data) {
+                        marked(value, function (err, data) {
                             setter($scope, data);
                         });
                     }
                 });
 
                 // 防止误操作
-                $scope.$on('$locationChangeStart', function(e) {
+                $scope.$on('$locationChangeStart', function (e) {
                     var content = $scope.$eval(options.src);  // 如果长度小于 minlength 为 undefined
                     if (content && !confirm('确定要离开？')) {
                         e.preventDefault();
@@ -54,26 +54,30 @@ define(function(require, exports, module) {
             /**
              * 停止监控
              */
-            stopPreview: function() {
+            stopPreview: function () {
                 preview = false;
+            },
+
+            startPreview: function () {
+                preview = true;
             },
 
             /**
              * 提供附件上传的功能，对于模板本身有一些要求
              * @param $scope
              */
-            addAttachFn: function($scope, parent) {
+            addAttachFn: function ($scope, parent) {
                 // 上传
-                $scope.doUpload = function(files) {
-                    _.each(files, function(file) {
+                $scope.doUpload = function (files) {
+                    _.each(files, function (file) {
                         var attach = Attach.create({originalFile: file});
                         parent.attaches.push(attach);
                     });
                     $scope.attachShow = true;
                 };
 
-                $scope.removeAttach = function(attach) {
-                    attach.$remove(function() {
+                $scope.removeAttach = function (attach) {
+                    attach.$remove(function () {
                         parent.attaches = _.without(parent.attaches, attach);
                         if (parent.attaches.length === 0) {
                             $scope.attachShow = false;
@@ -81,10 +85,10 @@ define(function(require, exports, module) {
                     });
                 };
 
-                $scope.showTip = function() {
+                $scope.showTip = function () {
                     $modal.open({
                         templateUrl: 'editor/tip',
-                        controller: ['$scope', '$modalInstance', function($scope, $modalInstance) {
+                        controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
                             $scope.modal = $modalInstance;
                         }]
                     });
