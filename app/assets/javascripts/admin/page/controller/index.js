978948f9d154b1efe5930738c138e1729a189e4d
edokeh <edokeh@163.com>
2014-01-22 17:30:32 +0800

    Try to fixing bug of page create/modify

--- app/assets/javascripts/admin/page/controller/index.js
+++ app/assets/javascripts/admin/page/controller/index.js
@@ -1,5 +1,11 @@
 /**
- * 页面列表
+ * 页面列表与编辑
+ * 复杂的业务规则：
+ * # 存在“编辑模式”
+ * # 只能修改、删除选中的页面
+ * # 同一时间，只能新建一个页面，新建页面时，立即选中并进入编辑模式
+ * # 正在编辑模式时，选中其他页面，则退出编辑模式，并还原数据
+ * # 退出编辑新建页面，则删除之
  */
 define(function(require, exports, module) {
     var angular = require('angularjs');
@@ -36,16 +42,16 @@ define(function(require, exports, module) {
 
         // 显示某一篇页面详情
         $scope.showPage = function(page) {
+            $scope.stopEdit();
             $scope.currPage = page;
-            if (!page) {
-                return;
+            if (page) {
+                if (page.id) {
+                    page.$get({detail: true});
+                }
+                else {
+                    $scope.startEdit(); // 如果显示的是新建页面，立刻进入编辑模式
+                }
             }
-            if (!page.id) {
-                $scope.startEdit();
-                return;
-            }
-            page.$get({detail: true});
-            $scope.cancelEdit();
         };
 
         // 是否是最后一个，需要排除新建的
@@ -54,21 +60,53 @@ define(function(require, exports, module) {
             return last === page || (!last.id && _.indexOf($scope.pages, page) === $scope.pages.length - 2);
         };
 
-        // 修改
+        // 进入编辑模式
+        var originalPage = {};
         $scope.startEdit = function() {
             $scope.editing = true;
             $scope.serverError = null;
+            angular.copy($scope.currPage, originalPage);  // 保存备份
             Editor.startPreview();
             Editor.addAttachFn($scope, $scope.currPage);
         };
 
-        $scope.cancelEdit = function() {
+        // 退出编辑模式
+        $scope.stopEdit = function() {
             $scope.editing = false;
             Editor.stopPreview();
-            // 退出编辑新建的页面时，删除它
+            return true;
+        };
+
+        // 放弃新建页面，并且删除它
+        $scope.dropCreate = function() {
+            $scope.stopEdit();
+            $scope.pages = _.without($scope.pages, $scope.currPage);
+            $scope.showPage($scope.pages[0]);
+        };
+
+        // 放弃修改页面，回复数据
+        $scope.dropUpdate = function() {
+            $scope.stopEdit();
+            if (originalPage.id) {
+                angular.copy(originalPage, $scope.currPage);
+            }
+        };
+
+        // 退出编辑模式，并放弃修改
+        $scope.dropEdit = function() {
+            if ($scope.stopEdit()) {
+                return;
+            }
+
             if (!$scope.currPage.id) {
                 $scope.pages = _.without($scope.pages, $scope.currPage);
                 $scope.showPage($scope.pages[0]);
+            }
+            // 已有的页面，丢弃变动
+            else {
+                if (originalPage.id) {
+                    angular.copy(originalPage, $scope.currPage);
+                }
             }
         };
 
@@ -97,7 +135,7 @@ define(function(require, exports, module) {
 
             if ($scope.form.$valid) {
                 $scope.currPage.$save(function() {
-                    $scope.cancelEdit();
+                    $scope.stopEdit();
                 }, function(resp) {
                     $scope.serverError = resp.data.errors;
                     showError();
