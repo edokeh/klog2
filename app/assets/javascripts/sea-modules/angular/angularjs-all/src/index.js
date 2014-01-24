a4f8c8cb1c70152c4da767a5894caa3bd952654d
edokeh <edokeh@163.com>
2014-01-24 15:41:26 +0800

    Add angular-all for js combine

---
+++ app/assets/javascripts/sea-modules/angular/angularjs-all/src/index.js
@@ -0,0 +1,16 @@
+define(function(require) {
+    var angular = require('angularjs');
+    var ngAnimate = require('angular-animate');
+    var ngResource = require('angular-resource');
+    var ngRoute = require('angular-route');
+    var ngSanitize = require('angular-sanitize');
+
+    var angularjsAll = angular.module('angularjs-all', [
+        ngAnimate.name,
+        ngRoute.name,
+        ngResource.name,
+        ngSanitize.name
+    ]);
+
+    return angular;
+});
