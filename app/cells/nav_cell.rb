303267d8151592e01612e0fe2d7dc6c877ccc263
edokeh <edokeh@163.com>
2014-01-27 15:32:48 +0800

    Fix bug of nav class

--- app/cells/nav_cell.rb
+++ app/cells/nav_cell.rb
@@ -1,8 +1,6 @@
 class NavCell < Cell::Rails
   helper FontAwesome::Rails::IconHelper, ApplicationHelper
 
-  cache :show, :expires_in => 5.minutes
-
   # 显示导航栏
   def show(curr_nav)
     @curr_nav = curr_nav
