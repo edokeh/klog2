1f2923790b9f5223e3be05b5514a22a537d1211a
edokeh <edokeh@163.com>
2014-02-10 00:10:53 +0800

    Modify dashboard

--- app/assets/javascripts/admin/dashboard/controller/index.js
+++ app/assets/javascripts/admin/dashboard/controller/index.js
@@ -1,136 +1,144 @@
 /**
  * Dashboard
  */
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     var _ = require('_');
     var Highcharts = require('angular-highcharts').Highcharts;
     var COLORS = Highcharts.getOptions().colors;
 
-    var IndexController = ['$scope', '$http', function($scope, $http) {
-        $http.get('/admin/dashboard').then(function(resp) {
+    var IndexController = ['$scope', '$http', function ($scope, $http) {
+        $http.get('/admin/dashboard').then(function (resp) {
             $scope.dashboard = resp.data;
-        });
 
-        $http.get('/admin/dashboard/top_pages').then(function(resp) {
-            $scope.topPages = resp.data;
-        });
+            // 如果没有启用 ga chart
+            if ($scope.dashboard.total_visits !== null) {
+                $scope.enable_chart = true;
 
-        $http.get('/admin/dashboard/hot_blogs').then(function(resp) {
-            $scope.hotBlogs = resp.data;
-        });
-
-        $http.get('/admin/dashboard/browser').then(function(resp) {
-            var data = resp.data;
-
-            var totalVisits = _.reduce(data, function(memo, i) {
-                return memo + i.visits;
-            }, 0);
-
-            var browserData = [];
-            var versionsData = [];
-
-            var groupData = _.groupBy(data, 'browser');
-            _.each(groupData, function(versions, browser) {
-                var versionTotalVisits = _.reduce(versions, function(memo, i) {
-                    return memo + i.visits;
-                }, 0);
-                var color = COLORS[browserData.length];
-                browserData.push({
-                    name: browser,
-                    y: toPercent(versionTotalVisits, totalVisits),
-                    color: color
+                $http.get('/admin/dashboard/top_pages').then(function (resp) {
+                    $scope.topPages = resp.data;
                 });
 
-                versionsData = versionsData.concat(versions.map(function(v, index) {
-                    var brightness = 0.2 - (index / versions.length) / 5;
-                    return {
-                        name: v.browser + ' ' + v.browserVersion,
-                        y: toPercent(v.visits, totalVisits),
-                        color: Highcharts.Color(color).brighten(brightness).get()
-                    };
-                }));
-            });
+                $http.get('/admin/dashboard/browser').then(function (resp) {
+                    var data = resp.data;
 
-            $scope.browser = {
-                title: {
-                    text: '浏览器占有率',
-                    margin: 25,
-                    style: chartTitleStyle()
-                },
-                plotOptions: {
-                    pie: {
-                        center: ['50%', '50%']
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                tooltip: {
-                    //headerFormat: '<span style="font-size: 10px;font-weight:bolder">{point.key:%b%e日 %A}</span><br/>'
-                    valueSuffix: '%'
-                },
-                series: [
-                    {
-                        name: 'Browsers',
-                        data: browserData,
-                        size: '60%',
-                        dataLabels: {
-                            formatter: function() {
-                                return this.y > 5 ? this.point.name : null;
-                            },
-                            color: 'white',
-                            distance: -40
-                        }
-                    },
-                    {
-                        name: 'Versions',
-                        data: versionsData,
-                        size: '80%',
-                        innerSize: '60%',
-                        dataLabels: {
-                            formatter: function() {
-                                // display only if larger than 1
-                                return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + '%' : null;
+                    var totalVisits = _.reduce(data, function (memo, i) {
+                        return memo + i.visits;
+                    }, 0);
+
+                    var browserData = [];
+                    var versionsData = [];
+
+                    var groupData = _.groupBy(data, 'browser');
+                    _.each(groupData, function (versions, browser) {
+                        var versionTotalVisits = _.reduce(versions, function (memo, i) {
+                            return memo + i.visits;
+                        }, 0);
+                        var color = COLORS[browserData.length];
+                        browserData.push({
+                            name: browser,
+                            y: toPercent(versionTotalVisits, totalVisits),
+                            color: color
+                        });
+
+                        versionsData = versionsData.concat(versions.map(function (v, index) {
+                            var brightness = 0.2 - (index / versions.length) / 5;
+                            return {
+                                name: v.browser + ' ' + v.browserVersion,
+                                y: toPercent(v.visits, totalVisits),
+                                color: Highcharts.Color(color).brighten(brightness).get()
+                            };
+                        }));
+                    });
+
+                    $scope.browser = {
+                        title: {
+                            text: '浏览器占有率',
+                            margin: 25,
+                            style: chartTitleStyle()
+                        },
+                        plotOptions: {
+                            pie: {
+                                center: ['50%', '50%']
                             }
-                        }
-                    }
-                ]
-            };
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        tooltip: {
+                            //headerFormat: '<span style="font-size: 10px;font-weight:bolder">{point.key:%b%e日 %A}</span><br/>'
+                            valueSuffix: '%'
+                        },
+                        series: [
+                            {
+                                name: 'Browsers',
+                                data: browserData,
+                                size: '60%',
+                                dataLabels: {
+                                    formatter: function () {
+                                        return this.y > 5 ? this.point.name : null;
+                                    },
+                                    color: 'white',
+                                    distance: -40
+                                }
+                            },
+                            {
+                                name: 'Versions',
+                                data: versionsData,
+                                size: '80%',
+                                innerSize: '60%',
+                                dataLabels: {
+                                    formatter: function () {
+                                        // display only if larger than 1
+                                        return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + '%' : null;
+                                    }
+                                }
+                            }
+                        ]
+                    };
+                });
+
+                $http.get('/admin/dashboard/daily_visits').then(function (resp) {
+
+                    $scope.daily_visits = {
+                        title: {
+                            text: '每日访问次数',
+                            margin: 25,
+                            style: chartTitleStyle()
+                        },
+                        xAxis: {
+                            type: 'datetime',
+                            labels: {
+                                format: '{value:%b%e日}'
+                            }
+                        },
+                        yAxis: {
+                            title: {
+                                text: ''
+                            },
+                            min: 0
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        tooltip: {
+                            headerFormat: '<span style="font-size: 10px;font-weight:bolder">{point.key:%b%e日 %A}</span><br/>'
+                        },
+                        series: [
+                            {
+                                name: "访问次数",
+                                data: resp.data
+                            }
+                        ]
+                    };
+                });
+            } else {
+                $scope.now = Highcharts.dateFormat('%Y %m %d', new Date().getTime())
+            }
         });
 
-        $http.get('/admin/dashboard/daily_visits').then(function(resp) {
 
-            $scope.daily_visits = {
-                title: {
-                    text: '每日访问次数',
-                    margin: 25,
-                    style: chartTitleStyle()
-                },
-                xAxis: {
-                    type: 'datetime',
-                    labels: {
-                        format: '{value:%b%e日}'
-                    }
-                },
-                yAxis: {
-                    title: {
-                        text: ''
-                    },
-                    min: 0
-                },
-                legend: {
-                    enabled: false
-                },
-                tooltip: {
-                    headerFormat: '<span style="font-size: 10px;font-weight:bolder">{point.key:%b%e日 %A}</span><br/>'
-                },
-                series: [
-                    {
-                        name: "访问次数",
-                        data: resp.data
-                    }
-                ]
-            };
+        $http.get('/admin/dashboard/hot_blogs').then(function (resp) {
+            $scope.hotBlogs = resp.data;
         });
 
         function toPercent(v, total) {
--- app/assets/javascripts/admin/dashboard/index.js
+++ app/assets/javascripts/admin/dashboard/index.js
@@ -8,7 +8,7 @@ define(function (require, exports, module) {
     angularHighcharts.Highcharts.setOptions({
         lang: {
             shortMonths: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-            weekdays: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期七']
+            weekdays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
         }
     });
 
