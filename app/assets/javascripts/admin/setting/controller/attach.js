52061cf9a775a067df71d3b65bb4e63d97904894
edokeh <edokeh@163.com>
2014-02-10 17:01:04 +0800

    Add statistic info in attach setting page

--- app/assets/javascripts/admin/setting/controller/attach.js
+++ app/assets/javascripts/admin/setting/controller/attach.js
@@ -4,10 +4,17 @@
 define(function(require, exports, module) {
     var angular = require('angularjs');
 
-    var Controller = ['$scope', 'Attach', 'RelativeUrlFactory', 'Confirm', '$location', '$routeParams', '$route', function($scope, Attach, RelativeUrlFactory, Confirm, $location, $routeParams, $route) {
+    var Controller = ['$scope', 'Attach', '$http', 'RelativeUrlFactory', 'Confirm', '$location', '$routeParams', '$route', function($scope, Attach, $http, RelativeUrlFactory, Confirm, $location, $routeParams, $route) {
         $scope.relativeUrl = RelativeUrlFactory.create(module);
         $scope.navClass = 'attach';
         var currPage;
+
+        // 获取统计
+        $scope.getStat = function() {
+            $http.get('/admin/dashboard/attach').then(function(resp) {
+                $scope.attachStat = resp.data;
+            });
+        };
 
         $scope.jumpPage = function(page) {
             $location.search('page', page);
@@ -22,17 +29,20 @@ define(function(require, exports, module) {
             Confirm.open('确定要删除“' + attach.file_name + '”？').then(function() {
                 attach.$remove(function() {
                     $scope.jumpPage(currPage);
+                    $scope.getStat();
                 });
             });
         };
-
-        $scope.jumpPage($routeParams.page || 1);
 
         $scope.$on('$routeUpdate', function() {
             if ($routeParams.page !== currPage) {
                 $scope.jumpPage($routeParams.page);
             }
         });
+
+        // 初始化
+        $scope.jumpPage($routeParams.page || 1);
+        $scope.getStat();
     }];
 
     Controller.title = '附件管理';
